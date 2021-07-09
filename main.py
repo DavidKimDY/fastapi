0f940e125b4546bb478def3aefd85ed2b408f68b
Ubuntu <ubuntu@ip-172-31-15-75.ap-northeast-2.compute.internal>
2021-07-09 16:50:12 +0900

    update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 import mongodb_util as mu
@@ -9,35 +10,62 @@ krx = mu.get_mongodb_collection('krx')
 krx_storage = mu.get_mongodb_collection('krx_storage')
 
 with open('.apikey', 'r') as f:
-    TEMP_API_KEY = f.read().rstrip('\n')
+    TEMP_API_KEYS = f.read().rstrip('\n').split()
 
+def log(msg):
+    time = datetime.datetime.today().isoformat()
+    with open('log.txt' , 'a') as f:
+        f.write(f'{time} - {msg} \n')
+
+def time_stamp():
+    return datetime.datetime.today().isoformat()
 
 @app.get('/')
 async def home():
+    time.sleep(10)
     return {'msg': 'Hi'}
-
 
 @app.get("/krx/stock/read/")
 async def krx_read(symbol: str, apikey: str):
-    now = time.time()
-    print('krx_read_start =', now)
-    if apikey != TEMP_API_KEY:
+    if apikey not in TEMP_API_KEYS:
+        log(f'{apikey} -  Wrong Api key')
         return {'msg' : 'Wrong Api key'}
-    print('before_mdb_requests = ', now)
     res = krx.find_one({'code': symbol})
-    print('after_mdb_requests = ', time.time())
     if res is None:
+        log(f'{apikey} - No data in database. symbol : {symbol}')
         return {'msg': 'No data in database'} 
+    log(f'{apikey} - krx_read ok, symbol : {symbol} ')
     return {'data': res['data']}
 
-
 @app.get("/krx/stock/readall/")
-async def krx_read_allr(date: str, apikey: str):
-    if apikey != TEMP_API_KEY:
+async def krx_read_date(date: str, apikey: str):
+    if apikey not in TEMP_API_KEYS:
+        log(f'{apikey} - wrong Api key')
         return {'msg' : 'Wrong Api key'}
     res = krx_storage.find_one({'date': date})
     if res is None:
+        log(f'{apikey} - No data in database. date : {date}')
         return {'msg': 'No data in database'} 
+    log(f'{apikey} - krx_read_date ok, date : {date}')
     return {'data': res['data']}
 
 
+def log_test(msg, n):
+    with open(f'test_log1/log_{n}.txt', 'a') as f:
+        f.write(msg + '\n')
+'''
+@app.get("/test/")
+async def test(symbol: str, number: str ):
+    log_test(f'{symbol} : Before requests to MongoDB server', number)
+    res = krx.find_one({'code': symbol})
+    log_test(f'{symbol} : After gets response from MongoDB server', number)
+    return {'data': res['data']}
+
+@app.get("/krx/stock/read/")
+async def krx_read(symbol: str, apikey: str):
+    number = apikey
+    log_test(f'{symbol} : Before requests to MongoDB server', number)
+    res = krx.find_one({'code': symbol})
+    log_test(f'{symbol} : After gets response from MongoDB server', number)
+    return {'data': res['data']}
+'''
